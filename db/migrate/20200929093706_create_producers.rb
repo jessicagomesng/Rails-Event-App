367ceb7d2305c1b2c71691295ffc6fe9a3be0f34
jessicagomesng <jgnpeluso@gmail.com>
2020-10-01 16:00:22 +0100

    updated migration files

--- db/migrate/20200929093706_create_producers.rb
+++ db/migrate/20200929093706_create_producers.rb
@@ -1,7 +1,6 @@
 class CreateProducers < ActiveRecord::Migration[6.0]
   def change
     create_table :producers do |t|
-      t.string :username
       t.string :email
       t.string :first_name
       t.string :last_name
