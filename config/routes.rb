a7767cc07a38d0103fd6dd43efd07fed1c2919ae
jessicagomesng <jgnpeluso@gmail.com>
2020-10-01 16:42:15 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'welcome#home'
+  get '/account', to: 'welcome#account'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+
+  resources :users do 
+    resources :events, only: [:index]
+  end 
+
+  resources :rsvps, only: [:create, :destroy]
+  
+  resources :producers do 
+    resources :events, only: [:index]
+  end 
+
+  resources :events do 
+    resources :users, only: [:index]
+  end 
+
+  resources :locations, except: [:destroy] do 
+    resources :events, only: [:new, :index]
+  end 
 end
