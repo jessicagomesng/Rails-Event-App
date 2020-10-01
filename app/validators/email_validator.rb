f8846304d8702c8f0854a5c737c9f0b9c1c095f3
jessicagomesng <jgnpeluso@gmail.com>
2020-10-01 16:53:07 +0100

    updated email validator

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,6 +1,6 @@
 class EmailValidator < ActiveModel::Validator 
     def validate(record)
-        if User.emails.include?(record.email.downcase) || Producer.emails.include?(record.email.downcase)
+        if record.email && (User.emails.include?(record.email.downcase) || Producer.emails.include?(record.email.downcase))
             record.errors[:email] << 'is already associated with an account.'
         end 
     end 
