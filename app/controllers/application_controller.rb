b8e96ddeae4e22f25c3d5e981b41dc366c1e8dbb
jessicagomesng <jgnpeluso@gmail.com>
2020-10-01 16:55:11 +0100

    added private methods to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+
+    private 
+    def current_user 
+        User.find_by_id(session[:user_id]) || Producer.find_by_id(session[:producer_id])
+    end 
+
+    def logged_in
+        !!current_user 
+    end 
+
+    def verified_user 
+        redirect_to root_path unless logged_in 
+    end 
+
+    def admin 
+        current_user.is_a? Producer 
+    end 
+
+    def permitted
+        redirect_to account_path unless admin 
+    end 
+
+    def account_redirect 
+        redirect_to account_path if logged_in 
+    end 
 end
