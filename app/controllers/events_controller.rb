eaef222f83a24e35b2a38f054c840accd08a8ba1
jessicagomesng <jgnpeluso@gmail.com>
2020-10-02 16:03:57 +0100

    added in helper methods

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,6 @@
 class EventsController < ApplicationController
+    before_action :permitted, only: [:new, :create, :edit, :update, :destroy]
+    before_action :set_event, only: [:show, :edit, :update, :destroy]
 
     def new 
     end 
