98b6d31055e43a419d039ea55ff4bf3162cb0cc9
jessicagomesng <jgnpeluso@gmail.com>
2020-10-02 16:10:33 +0100

    added new route

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,13 @@
 class EventsController < ApplicationController
-    before_action :permitted, only: [:new, :create, :edit, :update, :destroy]
-    before_action :set_event, only: [:show, :edit, :update, :destroy]
+    # before_action :permitted, only: [:new, :create, :edit, :update, :destroy]
+    # before_action :set_event, only: [:show, :edit, :update, :destroy]
 
     def new 
+        if params[:location_id]
+            @event = Event.new(:producer_id => session[:producer_id], :location_id => params[:location_id])
+        else 
+            @event = Event.new(:producer_id => session[:producer_id])
+        end 
     end 
 
     def create 
