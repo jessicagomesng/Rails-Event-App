2a933499405cea222160b071d5e839480c279495
jessicagomesng <jgnpeluso@gmail.com>
2020-10-03 16:58:59 +0100

    adding in sign-in methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,21 @@ class SessionsController < ApplicationController
     end 
 
     def create 
+        if params[:producer]
+            account_holder = Producer.find_by(:email => params[:email])
+        else 
+            account_holder = User.find_by(:email => params[:email])
+        end 
+
+        if account_holder && account_holder.authenticate(params[:password])
+            
+            session[(account_holder.class.name.downcase + "_id").to_sym] = account_holder.id
+            flash[:message] = "Successfully logged in!"
+            account_redirect
+        else 
+            flash[:message] = "Please try again."
+            redirect_to login_path  
+        end
     end 
 
     def destroy 
