3a5ee1a3b415baba66be225bb5dfc0d94fb21f31
jessicagomesng <jgnpeluso@gmail.com>
2020-10-03 17:23:49 +0100

    fixing rsvp method

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,4 +2,8 @@ module EventsHelper
     def author_admin(event)
         event.producer_id == current_user.id 
     end 
+
+    def waiting_list(event)
+        event.waiting_count > 0
+    end 
 end
