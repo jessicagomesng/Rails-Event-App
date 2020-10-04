d79ba87ac3c29443d7799b7177ce7be2f3961ce8
jessicagomesng <jgnpeluso@gmail.com>
2020-10-04 12:19:01 +0100

    rsvp#create method

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -1,5 +1,22 @@
 class RsvpsController < ApplicationController
     def create 
+        rsvp = Rsvp.new(:user_id => current_user.id, :event_id => params[:rsvp][:event_id], :status => nil)
+        @event = Event.find_by_id(params[:rsvp][:event_id])
+        @user = User.find_by_id(params[:rsvp][:user_id])
+
+        if rsvp.save 
+            if @event.full?
+                rsvp.update(:status => "waiting")
+                flash[:message] = "You have successfully joined the waiting list. You are number #{@user.place_in_line(@event)} in line."
+            else
+                rsvp.update(:status => "attending")
+                flash[:message] = "RSVP successful! You are attending this event."
+            end 
+            redirect_to user_events_path(@user)
+        else 
+            @rsvp = rsvp 
+            render "events/show"
+        end 
     end
 
     def destroy 
--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -2,6 +2,7 @@ class Rsvp < ApplicationRecord
     belongs_to :user
     belongs_to :event
 
+    validates :user_id, :event_id, :status, presence: true 
     validates_uniqueness_of :user_id, :scope => :event_id, :message => "has already signed up to this event/waiting list."
 
     scope :attending, -> { where(:status => "attending") }
