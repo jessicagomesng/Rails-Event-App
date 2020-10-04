4451e294cd581a5e879de088ea04670b448e0e8f
jessicagomesng <jgnpeluso@gmail.com>
2020-10-04 12:38:45 +0100

    added users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,15 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params)
+
+        if @user.save
+            session[:user_id] = @user.id 
+            flash[:message] = "Account created successfully."
+            account_redirect 
+        else 
+            render :New 
+        end 
     end 
 
     def index 
