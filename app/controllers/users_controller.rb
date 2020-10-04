5dee0625061798a638eece7d16c4ccab8e2a57c7
jessicagomesng <jgnpeluso@gmail.com>
2020-10-04 12:49:15 +0100

    adding users#edit and #update methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+
     def new
         @user = User.new 
     end
@@ -31,12 +33,28 @@ class UsersController < ApplicationController
     end 
 
     def show 
+        if !user_has_permission || !admin || !@user
+            flash[:message] = "Sorry. You do not have permission to access this page!"
+            account_redirect 
+        end 
     end
 
     def edit 
+        if !user_has_permission 
+            flash[:message] = "Sorry. You do not have permission to access this page!"
+            account_redirect
+        end 
     end 
 
     def update 
+        @user = current_user    
+
+        if @user.update(user_params)
+            flash[:message] = "User info updated successfully."
+            redirect_to user_path(@user)
+        else 
+            render :edit
+        end 
     end 
 
     def destroy 
@@ -45,5 +63,14 @@ class UsersController < ApplicationController
     private 
     def user_params
         params.require(:user).permit(:filter, :first_name, :last_name, :birthday, :email, :password, :password_confirmation)
+    end 
+
+    def set_user 
+        @user = User.find_by_id(params[:id])
+    end
+
+    def user_has_permission
+        #do i need this in producers? i can move it if so
+        @user == current_user
     end 
 end
