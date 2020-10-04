5f117e01d5e4f8650bfe95c4fe6a01f1918f8443
jessicagomesng <jgnpeluso@gmail.com>
2020-10-04 11:52:18 +0100

    adding new action for locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,9 +1,17 @@
 class LocationsController < ApplicationController
     
     def new
+        @location = Location.new
     end 
 
     def create 
+        @location = Location.new(location_params)
+
+        if @location.save
+            redirect_to location_path(@location)
+        else 
+            render :new 
+        end 
     end 
 
     def index 
