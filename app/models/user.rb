527d8fdc3cd36a757fcdaa7308545046ccb67459
jessicagomesng <jgnpeluso@gmail.com>
2020-10-04 13:46:02 +0100

    added omniauth creation method

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,4 +30,16 @@ class User < ApplicationRecord
     def events_waiting_for
         self.rsvps.waiting.collect { |rsvp| Event.find_by_id(rsvp.event_id) }
     end 
+
+    def self.from_omniauth(auth)
+        where(email: auth["info"]["email"]).first_or_create do |user|
+            user.id = auth["uid"]
+            user.first_name = auth["info"]["first_name"]
+            user.last_name = auth["info"]["last_name"]
+            user.email = auth["info"]["email"]
+            bday_hash = auth["extra"]["raw_info"]["birthday"].split("/").collect { |n| n.to_i }
+            user.birthday = Date.new(bday_hash[2], bday_hash[0], bday_hash[1])
+            user.password = user.password_confirmation = SecureRandom.urlsafe_base64(n=6)
+        end
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
+  get '/auth/facebook/callback', to: 'sessions#omniauth_user'
+
 
   resources :users do 
     resources :events, only: [:index]
