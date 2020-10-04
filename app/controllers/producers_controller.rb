ff7f0525b66064530216d533d793d9092e901051
jessicagomesng <jgnpeluso@gmail.com>
2020-10-04 12:58:11 +0100

    added producers#new

--- app/controllers/producers_controller.rb
+++ app/controllers/producers_controller.rb
@@ -6,6 +6,15 @@ class ProducersController < ApplicationController
     end 
     
     def create 
+        @producer = Producer.new(producer_params)
+        
+        if @producer.save 
+            session[:producer_id] = @producer.id
+            flash[:message] = "Account created successfully."
+            account_redirect
+        else 
+            render :new 
+        end 
     end
 
     def index 
@@ -24,7 +33,11 @@ class ProducersController < ApplicationController
     end 
 
     private
+    def producer_params
+        params.require(:producer).permit(:first_name, :last_name, :password, :password_confirmation, :email)
+    end 
+    
     def set_producer 
         @producer = Producer.find_by_id(params[:id])
-    end 
+    end
 end
