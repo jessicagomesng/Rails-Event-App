6bd656542086d4e5679335e502159dc43600929c
jessicagomesng <jgnpeluso@gmail.com>
2020-10-04 12:53:43 +0100

    added show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:show, :edit, :update, :destroy]
+    helper_method :user_has_permission
 
     def new
         @user = User.new 
