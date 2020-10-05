92880a79e6f2588e5738183d9b5a8796bcedc9a7
jessicagomesng <jgnpeluso@gmail.com>
2020-10-05 17:08:35 +0100

    added styling to flash messages

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,10 @@
 class WelcomeController < ApplicationController
     skip_before_action :verified_user, only: [:home]
-    before_account :account_redirect, only: [:home]
     
     def home 
+        if logged_in 
+            redirect_to account_path 
+        end
     end
 
     def account 
